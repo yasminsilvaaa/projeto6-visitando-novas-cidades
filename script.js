8162475d313042423912d960608eb8934fb1cdb7
yasmin silva <yasmin.nascimento50@aluno.ce.gov.br>
2024-09-12 09:33:07 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+function runTouristProgram() {
+    let touristName = prompt("Qual seu nome, turista?");
+    let citiesVisitedCount = 0;
+    let citiesList = "";
+    let hasVisitedCity = prompt("Você visitou alguma cidade? (s/n)").toLowerCase();
+
+    // Loop para coletar nomes das cidades visitadas
+    while (hasVisitedCity === 's') {
+        let cityName = prompt("Qual é o nome da cidade visitada?");
+        if (cityName) {
+            if (citiesVisitedCount > 0) {
+                citiesList += ", "; 
+            }
+            citiesList += cityName;
+            citiesVisitedCount++;
+        }
+        
+        hasVisitedCity = prompt("Você visitou alguma outra cidade? (s/n)").toLowerCase();
+    }
+    console.log(`Turista: ${touristName}`);
+    console.log(`Quantidade de cidades visitadas: ${citiesVisitedCount}`);
+    console.log("Cidades visitadas:");
+    console.log(citiesList);
+    
+    alert(`Turista: ${touristName}\nQuantidade de cidades visitadas: ${citiesVisitedCount}\nCidades visitadas: ${citiesList}`);
+}
+
+runTouristProgram();
