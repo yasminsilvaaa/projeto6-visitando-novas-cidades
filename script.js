e6ce13bd4f1b90361c7b8f6346a3502f11091e9f
mini tesuda <yasmin.nascimento50@aluno.ce.gov.br>
2024-09-16 16:34:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,17 @@
-function runTouristProgram() {
-    let touristName = prompt("Qual seu nome, turista?");
-    let citiesVisitedCount = 0
-    let citiesList = ""
-    let hasVisitedCity = prompt("Você visitou alguma cidade? (s/n)").toLowerCase();
+let tourist = prompt("Qual seu nome turista?")
+let city = prompt("Você visitou alguma cidade? (s/n)")
+let citylist = ""
+let numbercity = 0
 
-    // Loop para coletar nomes das cidades visitadas
-    while (hasVisitedCity === 's') {
-        let cityName = prompt("Qual é o nome da cidade visitada?");
-        if (cityName) {
-            if (citiesVisitedCount > 0) {
-                citiesList += ", "; 
-            }
-            citiesList += cityName;
-            citiesVisitedCount++;
-        }
-        
-        hasVisitedCity = prompt("Você visitou alguma outra cidade? (s/n)").toLowerCase();
-    }
-    console.log(`Turista: ${touristName}`);
-    console.log(`Quantidade de cidades visitadas: ${citiesVisitedCount}`);
-    console.log("Cidades visitadas:");
-    console.log(citiesList);
-    
-    alert(`Turista: ${touristName}\nQuantidade de cidades visitadas: ${citiesVisitedCount}\nCidades visitadas: ${citiesList}`);
+while(city == 's') {
+  if (numbercity > 0) {
+    citylist += ","
+  }
+citylist += prompt("Qual cidade voce visitou?")
+numbercity += 1
+city = prompt("Você visitou alguma outra cidade? (s/n)")
 }
 
-runTouristProgram();
+alert(`Turista: ${tourist}
+Quantidade de cidades visitadas: ${numbercity}
+Cidades visitadas: ${citylist}`)
